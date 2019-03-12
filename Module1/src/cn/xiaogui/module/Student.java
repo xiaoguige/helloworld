4fac43aa760930205e54c77d20d849a6d006eb5e
eoe <xiaogui@test.com>
2019-03-12 20:49:38 +0800

    Initial commit

---
+++ Module1/src/cn/xiaogui/module/Student.java
@@ -0,0 +1,7 @@
+package cn.xiaogui.module;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("hello World!!!");
+    }
+}
---
+++ src/cn/xiaogui/java/HelloWorld.java
@@ -0,0 +1,16 @@
+package cn.xiaogui.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+
+    public void method(){
+        System.out.println("sout");
+
+        List<String> list = new ArrayList<>();
+    }
+}
